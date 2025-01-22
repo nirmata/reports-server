92e4c777ad89056fd4ebcecac3f5e14e8a88d8d9
Amit Tiwari <amit.tiwari@nirmata.com>
2025-01-22 16:17:33 +0530

    fix cve

--- pkg/utils/constants.go
+++ pkg/utils/constants.go
@@ -2,17 +2,17 @@ package utils
 
 import (
 	reportsv1 "github.com/kyverno/kyverno/api/reports/v1"
-	"k8s.io/api/resource/v1alpha2"
+	v2 "k8s.io/api/autoscaling/v2"
 )
 
 var (
 	EphemeralReportsGR        = reportsv1.Resource("ephemeralreports")
 	ClusterEphemeralReportsGR = reportsv1.Resource("clusterephemeralreports")
-	PolicyReportsGR           = v1alpha2.Resource("policyreports")
-	ClusterPolicyReportsGR    = v1alpha2.Resource("clusterephemeralreports")
+	PolicyReportsGR           = v2.Resource("policyreports")
+	ClusterPolicyReportsGR    = v2.Resource("clusterephemeralreports")
 
 	EphemeralReportsGVK        = reportsv1.SchemeGroupVersion.WithKind("EphemeralReport")
 	ClusterEphemeralReportsGVK = reportsv1.SchemeGroupVersion.WithKind("ClusterEphemeralReport")
-	PolicyReportsGVK           = v1alpha2.SchemeGroupVersion.WithKind("PolicyReport")
-	ClusterPolicyReportsGVK    = v1alpha2.SchemeGroupVersion.WithKind("ClusterEphemeralReport")
+	PolicyReportsGVK           = v2.SchemeGroupVersion.WithKind("PolicyReport")
+	ClusterPolicyReportsGVK    = v2.SchemeGroupVersion.WithKind("ClusterEphemeralReport")
 )
