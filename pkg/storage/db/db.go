6bb215e195ec59fe6aa23d692adab2f490d6a5cf
Jigar Joshi <jigar.joshi@nirmata.com>
2025-04-22 15:07:59 +0530

    added the readQuery method in the storage db

--- pkg/storage/db/db.go
+++ pkg/storage/db/db.go
@@ -1,8 +1,13 @@
 package db
 
 import (
+	"context"
 	"database/sql"
+	"math/rand"
 	"sync"
+	"time"
+
+	"k8s.io/klog/v2"
 )
 
 type MultiDB struct {
@@ -16,4 +21,51 @@ func NewMultiDB(primaryDB *sql.DB, readReplicaDBs []*sql.DB) *MultiDB {
 		PrimaryDB:      primaryDB,
 		ReadReplicaDBs: readReplicaDBs,
 	}
+}
+
+func (m *MultiDB) ReadQuery(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error) {
+	m.Lock()
+	replicas := append([]*sql.DB(nil), m.ReadReplicaDBs...)
+	m.Unlock()
+
+	source := rand.NewSource(time.Now().UnixNano())
+	rng := rand.New(source)
+	rng.Shuffle(len(replicas), func(i, j int) { replicas[i], replicas[j] = replicas[j], replicas[i] })
+
+	for _, readReplicaDB := range replicas {
+		rows, err := readReplicaDB.Query(query, args...)
+		if err != nil {
+			klog.ErrorS(err, "failed to query read replica due to : ", err)
+			klog.Info("retrying with next read replica")
+			continue
+		}
+		return rows, nil
+	}
+
+	klog.Info("no read replicas available, querying primary db")
+	return m.PrimaryDB.Query(query, args...)
+}
+
+func (c *cpolrdb) ReadQueryRow(ctx context.Context, query string, args ...interface{}) *sql.Row {
+	c.Lock()
+	replicas := append([]*sql.DB(nil), c.readReplicaDBs...)
+	c.Unlock()
+
+	source := rand.NewSource(time.Now().UnixNano())
+	rng := rand.New(source)
+	rng.Shuffle(len(replicas), func(i, j int) { replicas[i], replicas[j] = replicas[j], replicas[i] })
+
+	for _, readReplicaDB := range replicas {
+		row := readReplicaDB.QueryRow(query, args...)
+		if row.Err() != nil {
+			klog.ErrorS(row.Err(), "failed to query read replica due to : ", row.Err())
+			klog.Info("retrying with next read replica")
+			continue
+		}
+
+		return row
+	}
+
+	klog.Info("no read replicas available, querying primary db")
+	return c.primaryDB.QueryRow(query, args...)
 }
